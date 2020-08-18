f6c6a9b5fc1b0981b2df970e9f98701b8897fb27
Tathagato <t.dasrishi1998@gmail.com>
2020-08-18 13:17:25 +0530

    nav

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function myFunction() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
   }
+}
+
+
